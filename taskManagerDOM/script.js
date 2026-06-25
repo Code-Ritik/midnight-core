1a612796bc4fe09c3e39d6ae67e1d8ceab6606f7
Ritik.dev <faleritik24@gmail.com>
2026-06-26 02:03:21 +0530

    Implement dark mode toggle and theme storage
    Added functionality for dark mode toggle and theme persistence.

--- taskManagerDOM/script.js
+++ taskManagerDOM/script.js
@@ -147,3 +147,23 @@ document.querySelector(".clearTask").addEventListener("click", () => {
     allTaskData = [];
     renderTasks();
 });
+
+
+const moon = document.querySelector(".modes");
+
+moon.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+});
+
+if (localStorage.getItem("theme") === "dark") {
+
+    document.body.classList.add("dark");
+}
